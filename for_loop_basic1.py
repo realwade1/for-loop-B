a144d3d5b93f5020030b2da5221d0ad17443a137
realwade1 <oliver.jordan199616@gmail.com>
2019-07-02 14:39:24 -0500

    first commit

---
+++ for_loop_basic1.py
@@ -0,0 +1,32 @@
+# #1 
+# for x in range(151):
+#     print (x)
+
+# #2 
+# for x in range(5, 1001, 5):
+#     print(x)
+
+# #3
+# for x in range (1,101):
+#     if(x % 10 == 0):
+#         print ("coding dojo")
+#     if(x % 5 == 0):
+#         print ("coding dojo")
+#     else: print(x)
+
+# #4
+# for x in range (500001):
+#     if (x % 2 != 0):
+#         print (x)
+
+# #5 
+# for x in range (2018,0,-4):
+#     print(x)
+
+# #6
+# lowNum = 0
+# highNum = 80 
+# mult = 10
+# for x in range (0,81,10):
+#     if (x % 10 == 0):
+#         print (x)
